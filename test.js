2e3d8cb04899e06aa58902616772015f2c586ffe
Enrique Maldonado <emaldonado@modyo.com>
2022-12-21 08:49:13 -0300

    fix: local test

--- test.js
+++ test.js
@@ -5,14 +5,20 @@ const originMeta = {
     commentFrom: 'Generate comments from sarif file',
   }
 
-const path = 'test/_stubs/trivy-sarif.json';
+const path = 'test/_stubs/snyk-sarif.json';
 const title = 'Reporte de Seguridad';
 const data = fs.readFileSync(`${path}`, 'utf8');
 const json = JSON.parse(data);
+const levelRegex = /\nSeverity: [A-Z]+/g;
 
 let levels = new Map();
 json.runs[0].results.forEach(result => {
-    const level = result.level != undefined ? result.level : 'undefined'
+    const text = result.message.text;
+    const levelText = text.match(levelRegex);
+    let level = levelText != null ? levelText.toString().split(' ')[1]: 'UNKNOWN'
+    if(level == 'UNKNOWN') {
+        level = result.level == undefined ? 'UNKNOWN' : result.level.toString().toUpperCase();
+    }
     const count = levels.has(level) ? levels.get(level) : 0;
     levels.set(level,count+1);
 });
@@ -26,5 +32,10 @@ for (const key of levels.keys()) {
     resume += `|${key.padEnd(30)}|${levels.get(key).toString().padStart(10)}|
 `
 };
+
+    // Now I add a coment for each issue in results
+    json.runs[0].results.forEach(result => { 
+        resume += `\n**${result.ruleId}**\n> ${result.message.text}\n`
+      });
 
 console.info(resume);
